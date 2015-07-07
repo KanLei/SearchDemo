425fab482cdfcf45ff0166dcb568351ca72230c8
Lei <kanlei@live.com>
2015-07-07 10:35:05 +0800

    init

---
+++ SearchDemo/ColorTableViewController.cs
@@ -0,0 +1,14 @@
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace SearchDemo
+{
+	partial class ColorTableViewController : UITableViewController
+	{
+		public ColorTableViewController (IntPtr handle) : base (handle)
+		{
+		}
+	}
+}
